ad1c149b2a00d69785f0d6ffe8c74766c497c611
Félix Carpena <felix@carpena.eu>
2022-03-07 21:49:12 +0100

    move controller to infrastructure

--- src/main/java/dev/carpena/usermanagement/infrastructure/ui/http/controller/UserController.java
+++ src/main/java/dev/carpena/usermanagement/infrastructure/ui/http/controller/UserController.java
@@ -1,4 +1,4 @@
-package dev.carpena.usermanagement.controller;
+package dev.carpena.usermanagement.infrastructure.ui.http.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
